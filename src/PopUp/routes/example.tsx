c24fa7cad367e5c16cb27999eb2627476b75935c
LordCat <kristianlexball@gmail.com>
2026-06-30 01:25:35 +0100

    Fixed Typo

--- src/PopUp/routes/example.tsx
+++ src/PopUp/routes/example.tsx
@@ -6,7 +6,7 @@ function Example(){
             <h1 className="text-3xl">Vite + React + Tailwindcss</h1>
                 <div className="card">
                 <p>
-                    Edit <code>src/PopUp/routes/example.jsx</code> and build your Pop Up Componenet here!
+                    Edit <code>src/PopUp/routes/example.tsx</code> and build your Pop Up Componenet here!
                     Check out 
                     {/* or even <a href={document.location.href} target="_blank">The webpage/dashboard</a> */}
                  </p>
