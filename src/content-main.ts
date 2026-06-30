9c8e2376359a4fe9d6ec0846c7b83e82c0cf424c
LordCat <kristianlexball@gmail.com>
2026-06-30 01:15:55 +0100

    Fixed Typo

--- src/content-main.ts
+++ src/content-main.ts
@@ -4,4 +4,4 @@ import { getCurrentURL } from './components/Content/getCurrent_URL';
 getCurrentURL();
 browser.runtime.sendMessage({ type: 'URL', payload: window.location.href });
 
-// You can add more initialization logic here if need
+// You can add more initialization logic here if needed.
